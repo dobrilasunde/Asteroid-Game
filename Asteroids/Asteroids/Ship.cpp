dac6e4e2a9d1be5e7e1dec7fdc2ba0101a459a46
DESKTOP-ENHSTHO\Flicka <dobrila.sunde@gmail.com>
2018-08-09 18:12:41 +0200

    Added ship and asteroid collision

--- Asteroids/Asteroids/Ship.cpp
+++ Asteroids/Asteroids/Ship.cpp
@@ -3,8 +3,11 @@
 #include"SpriteComponent.hpp"
 #include"InputComponent.hpp"
 #include"Laser.hpp"
+#include"CircleComponent.hpp"
+#include"Asteroid.hpp"
+#include<iostream>
 
-Ship::Ship(Game* game) : Actor(game), mLaserCooldown(0.0f)
+Ship::Ship(Game* game) : Actor(game), mLaserCooldown(0.0f), mSeconds(-1), mDisappear(false)
 {
 	SpriteComponent* sc = new SpriteComponent(this, 150);
 	sc->SetTexture(game->GetTexture("Assets/Ship.png"));
@@ -16,11 +19,23 @@ Ship::Ship(Game* game) : Actor(game), mLaserCooldown(0.0f)
 	ic->SetCounterClockwiseKey(SDL_SCANCODE_D);
 	ic->SetMaxForwardSpeed(300.0f);
 	ic->SetMaxAngularSpeed(Math::TwoPi);
+
+	mCircle = new CircleComponent(this);
+	mCircle->SetRadius(50.0f);
 }
 
 void Ship::UpdateActor(float deltaTime)
 {
 	mLaserCooldown -= deltaTime;
+
+	for (auto ast : GetGame()->GetAsteroids())
+	{
+		if (Intersect(*mCircle, *ast->GetCircle()))
+		{
+			SetPosition(Vector2(512.0f, 384.0f));
+			SetRotation(Math::PiOver2);
+		}
+	}
 }
 
 void Ship::ActorInput(const uint8_t* keyState)
--- Asteroids/Asteroids/Ship.hpp
+++ Asteroids/Asteroids/Ship.hpp
@@ -10,6 +10,11 @@ public:
 	void UpdateActor(float deltaTime) override;
 	void ActorInput(const uint8_t* keyState) override;
 
+	class CircleComponent* GetCircle() { return mCircle; }
+
 private:
 	float mLaserCooldown;
+	class CircleComponent* mCircle;
+	float mSeconds;
+	bool mDisappear;
 };
